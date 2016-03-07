e1b34233e53313131d3a033db6d0ecaf841ddde4
Dan Shahin <dan@codescience.com>
2016-03-06 17:43:48 -0800

    works for public repos

--- github.js
+++ github.js
@@ -1,57 +1,114 @@
-(function(){
-"use strict";
-//document.body.style.background = 'yellow';
+(function() {
+    "use strict";
+    //document.body.style.background = 'yellow';
 
-$(document).on('DOMNodeInserted',function(e){
-    var $pipeline = $(e.target);
-    //console.log(e.target);
-    if($pipeline.hasClass('zh-pipeline')){
-        //console.log('inserted issue pipeline',$pipeline);
-        var $wrapper = $pipeline.children('div.zh-pipeline-issues-wrapper');
-        // console.log('wrapper', $wrapper);
-        var $issues = $wrapper.children('ul.zh-pipeline-issues')
-            .each(function(){
-                var $issueList = $(this);
-                $issueList.children('li.zh-pipeline-issue')
-                    .each(function(){
-                        var $issue = $(this),
-                            $card = $issue.children('div.zh-issuecard-container');
+    var github = new Github();
+
+    var issueByNumber = {};
+
+    function indexIssues(){
+        var issues = github.getIssues('dshahin', 'jsr-mocks');
+
+        issues.list({}, function(err, issues) {
+            if(err){
+                console.error(err);
+            }else{
+                console.log(issues);
+                for(var i =0;  i< issues.length; i++){
+                    var issue = issues[i];
+                    console.log('issue', issue);
+                    issueByNumber[issue.number] = issue;
+                }
+                console.log('issueByNumber',issueByNumber);
+            }
+            
+        });
+    }
+
+    indexIssues();
+
+    function processPipeline(e){
+        var $element = $(e.target);
+        //console.log(e.target);
+        if ($element.hasClass('zh-pipeline')) {
+            var $pipeline = $element;
+            var $wrapper = $pipeline.children('div.zh-pipeline-issues-wrapper');
+
+            var $issues = $wrapper.children('ul.zh-pipeline-issues')
+                .each(function() {
+                    var $issueList = $(this);
+                    $issueList.children('li.zh-pipeline-issue')
+                        .each(function() {
+                            var $issue = $(this),
+                                $card = $issue.children('div.zh-issuecard-container');
                             // $main = $card.children('div.zh-issuecard-main');
                             // $meta = $card.children('div.zh-issuecard-meta');
 
-                            var max = 10,
-                                min = 0,
-                                totalTasks = Math.floor(Math.random() * (max - min + 1) + min),
-                                completeTasks = Math.floor(Math.random() * (totalTasks - min + 1) + min);
+                            var issueNumber = $card.children('div.zh-issuecard-main')
+                                .children('.zh-issuecard-body-container')
+                                .children('.zh-issuecard-header')
+                                .children('.zh-issuecard-number')
+                                .text().replace(/\s|\#/g,'');
 
-                            var percentage = ((completeTasks /totalTasks ) * 100).toFixed(0);
 
-                            var $progress = $('<div/>').addClass('zh-issue-progress').addClass('orange');
-                            var $meter = $('<div/>').addClass('meter');
+                            var issue = issueByNumber[issueNumber];
+                            
+                            if(issue){
+                                var body = issue.body;
+                                
+                                var totalTasks  = (body.match(/- \[[\s|x|X]\]/g) || []).length,
+                                    completeTasks = (body.match(/- \[[x|X]\]/g) || []).length ;
 
-                            $card.append($progress);
+                                var max = 10,
+                                    
+                                    min = 0,
+                                    
+                                    //totalTasks = Math.floor(Math.random() * (max - min + 1) + min),
+                                    
+                                    //completeTasks = Math.floor(Math.random() * (totalTasks - min + 1) + min),
+                                    
+                                    percentage = ((completeTasks / totalTasks) * 100).toFixed(0),
+                                    
+                                    $progress = $('<div/>').addClass('zh-issue-progress'),
+                                    
+                                    $meter = $('<div/>').addClass('meter'),
+                                    
+                                    label = $('<label/>').html(completeTasks + '/' + totalTasks + ' (' + percentage + '%)'),
+                                    
+                                    span = $('<span/>')
+                                        .addClass('zh-issue-progress')
+                                        .css({ width: percentage + '%' });
+                                
+                                if (totalTasks > 0) {
+                                    
+                                    $card.append($progress);
 
-                            if(totalTasks > 0){
-                                $progress.append($meter);
+                                    $progress.append($meter);
+                         
+                                    $progress.append(label);
+                               
+                                    $meter.append(span);
+                                }
+
                             }
 
-                            var spanNumber = $('<label/>').html(completeTasks + '/' +totalTasks +' ('+percentage +'%)');
-                            var span = $('<span/>')
-                                .addClass('zh-issue-progress')
-                                .css({ width: percentage + '%'});
-                                //.html(completeTasks + '/' +totalTasks);
-                            if(totalTasks > 0){
-                                $progress.append(spanNumber);
-                            }
-
-                            if(totalTasks > 0){
-                                $meter.append(span);
-                            }
-
-
-
-                    });
-            });
+                        });
+                });
+        }
     }
-});
+
+    $(document).on('DOMNodeInserted', function(e){
+
+        var $element = $(e.target);
+        if ($element.hasClass('zh-pipeline')) {
+            processPipeline(e);
+        }else if($element.hasClass('zh-pipeline-issue')){
+            console.log('just an issue');
+            $('.zh-pipeline').each(function(){
+                var $pipe = $(this);
+                console.log('pipe',$pipe);
+                processPipeline($pipe.get());
+            });
+        }
+    });
 })();
